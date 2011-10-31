b2c99f0a24ed1d6d75f96ff4ee8655520bbd0f15
Beth Werbaneth <werbae@rpi.edu>
2011-10-30 20:08:31 -0400

    added the bomb weapon (no way to test it just yet)

--- projectiles.py
+++ projectiles.py
@@ -40,6 +40,7 @@ class Projectile(object):
 		cNodePath = self.form.attachNewNode(cNode)
 		base.cTrav.addCollider(cNodePath, self.cHandler)
 	
+	
 	def update(self, task):
 		"""moves the bullet in a straight line relative to its trajectory"""
 		elapsed = task.time - self.prevtime
@@ -49,12 +50,75 @@ class Projectile(object):
 		
 		#check to see if the projectile is out of range
 		if math.sqrt((self.origx-self.xpos)^2 + (self.origy-self.ypos)^2) > self.range:
+			 #kill the projectile
+			 self.form.removeNode()
+		
+		self.prevtime = task.time
+		return Task.cont
 			
 	def kill(self, cEntry):
 		"""destroys the bullet upon entering a foreign body"""
-		cEntr.getIntoNodePath().getParent().remove()
+		cEntry.getIntoNodePath().getParent().remove()
 	
+class Bomb(DirectObject):
+	def __init__(self, x, y, z, angle):
+		self.xpos = x
+		self.ypos = y
+		self.zpos = z
+		self.angle = angle
+		self.loadModel()
+		self.setupCollisions()
+		self.prevtime = -1
+		self.countdown = 3.0
+		self.exploderange = 10.0
+		self.accept('bomb-detonated-player', self.explode)
+	
+	def loadModel(self):
+		"""loads the bomb model"""
+		self.form = loader.loadModel("bombproxy")
+		self.form.reparentTo(render)
 		
+	def setupCollisions(self):
+		"""sets the bomb up to collide with things"""
+		base.cTrav = CollisionTraverser()
+		self.cHandler = CollisionHandlerEvent()
+		self.cHandler.setInPattern('bomb-detonated-%in')
+		
+		cSphere = CollisionSphere((0,0,0), 1)
+		cNode = CollisionNode("bomb")
+		cNode.addSolid(cSphere)
+		cNodePath = self.form.attachNewNode(cNode)
+		base.cTrav.addCollider(cNodePath, self.cHandler)
+		
+	def update(self, task):
+		"""the bomb sits until someone runs into it or its timer goes off"""
+		if self.prevtime == -1:
+			elapsed = 0
+		else:
+			elapsed = task.time - self.prevtime
+		
+		self.countdown = self.countdown - elapsed
+		
+		if self.countdown <= 0:
+			self.explode()
+			self.form.removeNode()
+		
+		self.prevtime = task.time
+		return Task.cont
+	
+	def explode(self):	
+		"""the bomb explodes"""
+		base.cTrav = CollisionTraverser()
+		self.cHandler = CollisionHandlerEvent()
+		self.cHandler('blew-up-%in')
+		
+		explosionSphere = CollisionSphere(self.xpos, self.ypos, self.zpos, self.exploderange)
+		cNode = CollisionNode("explosion")
+		Cnode.addSolid(explosionSphere)
+		cNodePath = self.form.attachNewNode(cNode)
+		base.cTrav.addCollider(cNodePath, self.cHandler)
+		
+	
 #class Bullet(Projectile):
 	
 	
--- weapons.py
+++ weapons.py
@@ -15,7 +15,7 @@ class Weapon(DirectObject):
 		self.accept("space", self.setKey, ["firing", 1] )
 		self.accept("space-up", self.setKey, ["firing", 0] )
 		self.projectiles = []
-		self.cooldown = 1
+		self.cooldown = 1.0
 		
 		self.angle = angle
 		self.xpos = x
@@ -30,6 +30,9 @@ class Weapon(DirectObject):
 	def setKey(self, key, value):
 		self.keyMap[key] = value
 	
+	
+	#note: angle is the angle that the player is facing
+	#x, y, is the horizontal plane
 	def update(self, x, y, z, angle, elapsed):
 		"""you need to call update on the weapon every time you update player"""
 		self.angle = angle
@@ -54,9 +57,33 @@ class Weapon(DirectObject):
 	def fire(self):
 		"""pull the trigger"""
 		new_projectile = projectile(5, x, y, z, angle, self.range)
-		self.projectiles.append(new_projectiles)
+		self.projectiles.append(new_projectile)
 		self.cooldown = 1.0
 		
+	def kill(self):
+		"""removes the weapon from the scene"""
+		pass
+	
+		
+class BombWeapon(Weapon):
+	def __init__(self, x, y, z, angle):
+		Weapon.__init__(self, x, y, z, angle)
+		self.cooldown = 5.0
+	
+	#each individual method is going to need to load its own model
+	def LoadModel(self):
+		pass
+	
+	def fire(self):
+		"""drops a bomb"""
+		#note: bombs don't inherit from projectile class
+		new_bomb = bomb(x, y, z, angle)
+		self.projectiles.append(new_bomb)
+		self.cooldown = 5.0
+	
+
+	
+	
 		
 		
 		
