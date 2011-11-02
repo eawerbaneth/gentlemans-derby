98561c40305fcc2bea910d5b7fcb7370af2a25e7
Beth Werbaneth <werbae@rpi.edu>
2011-11-02 15:24:26 -0400

    spikes working

--- obstacles.py
+++ obstacles.py
@@ -14,7 +14,7 @@ class Spikes(DirectObject):
 		self.ypos = y
 		self.zpos = z
 		self.loadModel()
-		self.setupCollisions():
+		self.setupCollisions()
 		self.prevtime = 0
 		self.penalty = 4
 		taskMgr.add(self.update, "spike_update")
@@ -22,16 +22,16 @@ class Spikes(DirectObject):
 	def loadModel(self):
 		"""loads the spikes"""
 #FLAG: waiting on spikes model
-	self.form = loader.loadModel("models/panda-model")
-	self.form.setScale(.02)
-	self.form.reparentTo(render)
+		self.form = loader.loadModel("models/panda-model")
+		self.form.setScale(.02)
+		self.form.reparentTo(render)
 		
 	def setupCollisions(self):
 		#base.cTrav = CollisionTraverser()
 		self.cHandler = CollisionHandlerEvent()
 		self.cHandler.setInPattern("spiked-%in")
 		
-		cQuad = CollisionPolygon(Point3(0, 0, 0), Point3(0, 0, 10), Point3(0, 10, 10), Point 3(0, 10, 0))
+		cQuad = CollisionPolygon(Point3(0, 0, 0), Point3(0, 0, 10), Point3(0, 10, 10), Point3(0, 10, 0))
 		cNode = CollisionNode("spikes")
 		cNode.addSolid(cQuad)
 		cNodePath = self.form.attachNewNode(cNode)
@@ -41,15 +41,15 @@ class Spikes(DirectObject):
 		elapsed = task.time - self.prevtime
 		#if mode is 0, move up
 		if self.mode == 0:
-			self.form.setPos(self.form.getX(), self.form.getY(), self.form.getZ()+1*elapsed)
+			self.form.setPos(self.form.getX(), self.form.getY(), self.form.getZ()+3*elapsed)
 			#if we reach the top, set mode to 1
-			if self.form.getZ() > zpos+10:
+			if self.form.getZ() > self.zpos+10:
 				self.mode = 1
 		#else move down
 		else:
-			self.form.setPos(self.form.getX(), self.form.getY(), self.form.getZ()-1*elapsed)
+			self.form.setPos(self.form.getX(), self.form.getY(), self.form.getZ()-3*elapsed)
 			#if we reach the bottom, set mode to 0
-			if self.form.getZ() < zpos:
+			if self.form.getZ() < self.zpos-10:
 				self.mode = 0
 		
 		self.prevtime = task.time
